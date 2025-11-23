f7d549c617634baec42042e3832a5ae4e8c6427d
jiang <2035645950@qq.com>
2025-11-23 16:24:26 +0800

    firstcommit

---
+++ src/main/java/com/example/cloudoffice_admin/CloudOfficeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudoffice_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudOfficeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudOfficeAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloudoffice_admin/CloudOfficeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudoffice_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudOfficeAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
